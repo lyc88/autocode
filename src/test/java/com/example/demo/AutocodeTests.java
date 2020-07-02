5d0156160a4e2f782fa201cfe789f06ca7cfe29c
lyc984006207 <984006207@qq.com>
2020-07-02 11:32:52 +0800

    真加了扫包 工具

--- src/test/java/com/example/demo/AutocodeTests.java
+++ src/test/java/com/example/demo/AutocodeTests.java
@@ -4,11 +4,16 @@ import com.example.demo.service.MysqlAutoService;
 import freemarker.template.TemplateException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Set;
 
 /**
  * Author:   lyc
@@ -24,5 +29,24 @@ public class AutocodeTests {
     @Test
     public void contextLoads() throws IOException, TemplateException {
         mysqlAutoService.autoCode("user");
+    }
+
+    @Test
+    public void test(){
+        Reflections reflections = new Reflections("com.example.demo");
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(RestController.class);
+        annotated.forEach(e->{
+            System.out.println(e.getName());
+            Method[] methods = e.getMethods();
+            for (Method method:methods){
+                //System.out.println("method:" +method.getName());
+                RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                if(annotation != null){
+                    String[] value = annotation.value();
+                    if(value != null && value.length>=1)
+                        System.out.println("value:" +value[0]);
+                }
+            }
+        });
     }
 }
