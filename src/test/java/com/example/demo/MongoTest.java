b1805b1bec9ea18e537d85dc8c37073a5353b9ab
lyc <984006207@qq.com>
2020-08-01 17:03:00 +0800

    代码生成器优化

--- src/test/java/com/example/demo/MongoTest.java
+++ src/test/java/com/example/demo/MongoTest.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -25,10 +28,25 @@ public class MongoTest {
     @Test
     public void test(){
         Query query = new Query();
-        query.addCriteria(Criteria.where("_id").is(0).and("name").is("瓜田李下1"));
+        //query.addCriteria(Criteria.where("_id").is(0).and("name").is("瓜田李下1"));
        //query.addCriteria(Criteria.where("name").is("瓜田李下1"));
+        query.addCriteria(Criteria.where("_id").is(0).orOperator(Criteria.where("name").regex("瓜田李下1"),Criteria.where("name").regex("瓜田李下")));
+        /*Criteria criteria = new Criteria();
+        criteria.orOperator(Criteria.where("name").regex("瓜田李下1"),Criteria.where("name").regex("瓜田李下"));
+        query.addCriteria(criteria);
+        query.addCriteria(Criteria.where("_id").is(0));*/
 
         List<Map> maps = mongoTemplate.find(query, Map.class, "person");
         System.out.println(maps);
+
+        // 分组 根据 年龄分组 统计 年龄多少人 并且知道是那些人
+        Aggregation aggregation = Aggregation.newAggregation(
+                Aggregation.group("age").count().as("count")
+                        .addToSet("name").as("name")
+                        .first("age").as("age")
+        );
+        //aggregation.withOptions(Sort.by("age"));
+        AggregationResults<Map> ans = mongoTemplate.aggregate(aggregation, "person", Map.class);
+        System.out.println("query: " + aggregation + " | groupQuery " + ans.getMappedResults());
     }
 }
