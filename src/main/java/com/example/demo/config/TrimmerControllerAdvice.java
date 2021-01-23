e7b7cde7fc04938aacd3976f405c6798a3dbd554
lyc88 <984006207@qq.com>
2021-01-23 21:57:13 +0800

    統一異常

--- src/main/java/com/example/demo/config/TrimmerControllerAdvice.java
+++ src/main/java/com/example/demo/config/TrimmerControllerAdvice.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * @author lyc
  * @date 2019/10/21.
+ *
+ * https://www.jianshu.com/p/a07d21baf6c3 統一異常
  */
 @RestControllerAdvice
 public class TrimmerControllerAdvice {
