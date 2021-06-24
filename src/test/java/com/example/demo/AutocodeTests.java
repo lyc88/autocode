c3f229a04d4cba2b12838eadfa74cb98ac181ff5
liyichuan <yichuan.li@vodsen.com>
2021-06-24 18:11:08 +0800

    多模块多路径 代码生成

--- src/test/java/com/example/demo/AutocodeTests.java
+++ src/test/java/com/example/demo/AutocodeTests.java
@@ -36,7 +36,7 @@ public class AutocodeTests {
     private UserService userService;
     @Test
     public void contextLoads() throws Exception {
-        autoCode.autoCode("app_version");
+        autoCode.autoCode("ums_user_device_rel");
        /* Date date = DateUtil.parse("2021-04-19", "yyyy-MM-dd");
         System.out.println("before"+date.before(new Date()));
 
